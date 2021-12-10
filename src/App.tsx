b2d249fb8a780e77747b6c4e2ad1d9c93c9facfc
programmer-shaheb <abidurrahman471@gmail.com>
2021-12-10 21:30:56 +0600

    react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
+  const [reservationNameInput, setReservationNameInput] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="reservation-container">
+          <div>
+            <h5 className="reservation-header">Reservations</h5>
+            <div className="reservation-cards-container">
+              {reservations.map((name, index) => {
+                return <ReservationCard name={name} index={index} />;
+              })}
+            </div>
+          </div>
+          <div className="reservation-input-container">
+            <input
+              value={reservationNameInput}
+              onChange={(e) => {
+                setReservationNameInput(e.target.value);
+              }}
+            />
+            <button onClick={handleAddReservations}>Add</button>
+          </div>
+        </div>
+        <div className="customer-food-container">
+          {customers.map((customer) => {
+            return (
+              <CustomerCard
+                id={customer.id}
+                name={customer.name}
+                food={customer.food}
+                key={customer.id}
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
